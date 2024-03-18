6850fd3a87bbfb67c3915a90fa9ebded93faf3e6
Miguel Gisbert <miguel.gisbert@bluecodesolutions.com>
2024-03-18 18:24:35 +0100

    Completing login and signup + favicon

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,18 +1,19 @@
 import { useState, useEffect } from 'react'
 import { auth } from '../firebaseConfig'
-import { onAuthStateChanged, User } from 'firebase/auth'
+import { onAuthStateChanged, User, signOut } from 'firebase/auth'
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth'
 import { FirebaseError } from 'firebase/app'
 import { Button, Box, TextField, Popper, Typography } from '@mui/material'
+import LogoutIcon from '@mui/icons-material/Logout'
+import { set } from 'firebase/database'
 
 function Login() {
 
-  console.log(auth)
-
-  const [showLogin, setShowLogin] = useState<boolean>(false)
+  const [formToShow, setFormToShow] = useState<'login' | 'signup' | 'none'>('none')
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
   const [errorMessage, setErrorMessage] = useState<string>('')
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
   const [user, setUser] = useState<User | null>(null)
 
@@ -22,14 +23,14 @@ function Login() {
       setEmail('')
       setPassword('')
       setErrorMessage('')
-    });
-
+    })
     // Cleanup subscription on unmount
     return () => unsubscribe()
   }, []);
 
   const signIn = async () => {
-
+    setFormToShow('none')
+    setAnchorEl(null)
     try {
       await signInWithEmailAndPassword(auth, email, password)
       console.log("User signed in successfully.")
@@ -52,23 +53,28 @@ function Login() {
   };
 
   const signUp = async () => {
+    setFormToShow('none')
+    setAnchorEl(null)
     try {
       console.log(email, password)
-      console.log(auth)
       await createUserWithEmailAndPassword(auth, email, password)
       console.log("User signed up successfully.")
     } catch (error) {
       if (error instanceof Error) {
         const firebaseError = error as FirebaseError
+        console.log(email, password)
         switch (firebaseError.code) {
           case 'auth/email-already-in-use':
-            setErrorMessage('Aquest correu electrònic ja està en ús.')
-            break;
-          case 'auth/invalid-email':
-            setErrorMessage('El correu electrònic introduït no és vàlid.')
+            setErrorMessage('Email already in use.')
             break;
           case 'auth/weak-password':
-            setErrorMessage('La contrasenya introduïda és massa feble.')
+            setErrorMessage('Weak password (6 characters).')
+            break;
+          case 'auth/invalid-email':
+            setErrorMessage('Invalid email.')
+            break;
+          case 'auth/invalid-credential':
+            setErrorMessage('Wrong password.')
             break;
           default:
             setErrorMessage('S\'ha produït un error desconegut.')
@@ -76,32 +82,48 @@ function Login() {
         console.log(firebaseError.code, firebaseError.message)
       }
     }
-  };
+  }
 
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      console.log("User signed out successfully.")
+    } catch (error) {
+      console.log("Error signing out:", error);
+    }
+  }
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    setAnchorEl(anchorEl ? null : event.currentTarget);
-    setShowLogin(!showLogin);
-  };
+
+  const toggleLoginForm = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setAnchorEl(event.currentTarget)
+    setFormToShow(formToShow === 'login' ? 'none' : 'login')
+  }
+
+  const toggleSignUpForm = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setAnchorEl(event.currentTarget)
+    setFormToShow(formToShow === 'signup' ? 'none' : 'signup')
+  }
 
   return (
     <>
       {!user ? (
         <>
-          <Button color="inherit" onClick={handleClick}>Login</Button>
-          <Button color="inherit" onClick={signUp} sx={{ lineHeight: 1.3 }}>Create <br /> Account</Button>
+          <Button color="inherit" onClick={toggleLoginForm}>Login</Button>
+          <Button color="inherit" onClick={toggleSignUpForm} sx={{ lineHeight: 1.3 }}>Create <br /> Account</Button>
         </>
       ) : (
-        <p>{user.email}</p>
+        <Box gap={2} sx={{ display: "flex", flexDirection: "row" }}>
+          <p>{user.email}</p>
+          <Button><LogoutIcon onClick={logout} /></Button>
+        </Box>
       )}
-      <Popper open={showLogin} anchorEl={anchorEl} placement="bottom-end" sx={{ boxShadow: 5, padding:"40px", width:"340px", backgroundColor: "white" }}>
+      <Popper open={formToShow === 'login' || formToShow === 'signup'} anchorEl={anchorEl} placement="bottom-end" sx={{ boxShadow: 5, padding:"40px", width:"340px", backgroundColor: "white" }}>
         <Box  component="form" 
               sx={{ 
                 backgroundColor: "white", 
                 width:"100%", 
-                display: 'flex', 
-                flexDirection: 'column', 
+                display: "flex", 
+                flexDirection: "column", 
                 padding: 0,
                 gap: 2 
               }} 
@@ -110,15 +132,20 @@ function Login() {
             required
             id="email_login"
             label="Email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
           />
           <TextField
             required
             id="password_login"
             label="Password"
             type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
           />
         </Box>
-        <Button onClick={signIn}>Sign In</Button>
+        {formToShow === 'login' && <Button onClick={signIn}>Sign In</Button>}
+        {formToShow === 'signup' && <Button onClick={signUp}>Create Account</Button>}
         <Typography>{errorMessage}</Typography>
       </Popper>
       {/* <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
